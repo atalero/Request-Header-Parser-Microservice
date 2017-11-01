90803cad5a38deff12a628992402647d3117b301
Glitch (header-parser2) <none>
2017-11-01 20:47:08 +0000

    😜🏚 Updated with Glitch

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
-/*
-User Story: I can get the IP address, language and operating system for my browser.
-*/
 var express = require('express');
 var app = express();
-//var ip = req.header('x-forwarded-for') || req.connection.remoteAddress;
+var parser = require('./parser')
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
-  response.send('Welcome to the website');
-  console.log(request.headers);
+  response.sendFile(__dirname + '/views/index.html');
 });
 
+app.get('/api', function(request,response){
+  response.status(200).send(parser(request));
+});
 
-
-// listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
--- src/app.js
+++
@@ -1,6 +0,0 @@
-var express = require('express');
-var app = express();
-
-app.get('/api/whoami', function (req,res){
-  res.status(200).send('Hello World!');
-});
